e5dde434db1fdededcbd4e63c610d8425abd0912
EastShine <eastshine_@naver.com>
2023-08-28 01:20:07 +0900

    [add] 블로그 View 구현 (Thymeleaf)

---
+++ src/main/java/com/eastshine/springbootdev/dto/ArticleListViewResponse.java
@@ -0,0 +1,19 @@
+package com.eastshine.springbootdev.dto;
+
+import com.eastshine.springbootdev.domain.Article;
+import lombok.Getter;
+
+@Getter
+public class ArticleListViewResponse {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    public ArticleListViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+
+}
