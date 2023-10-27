8643c1dfef2daa817538889ded563b028a4281fd
eastshine <eastshine@pointi.com>
2023-10-27 16:45:33 +0900

    Feat: JWT 토큰 활용 로그인/로그아웃 로직 추가
    JWT AccessToken, RefreshToken 발급 및 테스트 작성

---
+++ src/main/java/com/eastshine/springbootdev/controller/TokenApiController.java
@@ -0,0 +1,24 @@
+package com.eastshine.springbootdev.controller;
+
+import com.eastshine.springbootdev.dto.CreateAccessTokenRequest;
+import com.eastshine.springbootdev.dto.CreateAccessTokenResponse;
+import com.eastshine.springbootdev.service.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class TokenApiController {
+    private final TokenService tokenService;
+
+    @PostMapping("/api/token")
+    public ResponseEntity<CreateAccessTokenResponse> createNewAccessToken(@RequestBody CreateAccessTokenRequest request) {
+        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(new CreateAccessTokenResponse(newAccessToken));
+    }
+}
---
+++ src/main/java/com/eastshine/springbootdev/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.eastshine.springbootdev.repository;
+
+import com.eastshine.springbootdev.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+
+}
